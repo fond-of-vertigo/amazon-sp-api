507d3364857deb0d79f9ee8bf9b378650d059525
Leander Kohler <leander.kohler@fondof.de>
2023-01-10 14:05:09 +0100

    Remove rate limiter from orders api

--- apis/orders/orders.go
+++ apis/orders/orders.go
@@ -5,10 +5,8 @@ import (
 	"errors"
 	"github.com/fond-of-vertigo/amazon-sp-api/apis"
 	"github.com/fond-of-vertigo/amazon-sp-api/httpx"
-	"golang.org/x/time/rate"
 	"net/http"
 	"net/url"
-	"time"
 )
 
 const pathPrefix = "/orders/v0"
@@ -38,30 +36,12 @@ type API interface {
 }
 
 type api struct {
-	HttpClient                        httpx.Client
-	RateLimitGetOrders                *rate.Limiter
-	RateLimitGetOrder                 *rate.Limiter
-	RateLimitGetOrderBuyerInfo        *rate.Limiter
-	RateLimitGetOrderAddress          *rate.Limiter
-	RateLimitGetOrderItems            *rate.Limiter
-	RateLimitGetOrderItemsBuyerInfo   *rate.Limiter
-	RateLimitUpdateShipmentStatus     *rate.Limiter
-	RateLimitGetOrderRegulatedInfo    *rate.Limiter
-	RateLimitUpdateVerificationStatus *rate.Limiter
+	HttpClient httpx.Client
 }
 
 func NewAPI(httpClient httpx.Client) API {
 	return &api{
-		HttpClient:                        httpClient,
-		RateLimitGetOrders:                rate.NewLimiter(rate.Every(time.Microsecond*16700), 20),
-		RateLimitGetOrder:                 rate.NewLimiter(rate.Every(time.Microsecond*16700), 20),
-		RateLimitGetOrderBuyerInfo:        rate.NewLimiter(rate.Every(time.Microsecond*16700), 20),
-		RateLimitGetOrderAddress:          rate.NewLimiter(rate.Every(time.Microsecond*16700), 20),
-		RateLimitGetOrderItems:            rate.NewLimiter(rate.Every(time.Second/2), 30),
-		RateLimitGetOrderItemsBuyerInfo:   rate.NewLimiter(rate.Every(time.Second/2), 30),
-		RateLimitUpdateShipmentStatus:     rate.NewLimiter(rate.Every(time.Second*5), 15),
-		RateLimitGetOrderRegulatedInfo:    rate.NewLimiter(rate.Every(time.Second/2), 30),
-		RateLimitUpdateVerificationStatus: rate.NewLimiter(rate.Every(time.Second/2), 30),
+		HttpClient: httpClient,
 	}
 }
 
@@ -75,25 +55,21 @@ func (a *api) GetOrders(filter *GetOrdersFilter) (*apis.CallResponse[GetOrdersRe
 
 	return apis.NewCall[GetOrdersResponse](http.MethodGet, pathPrefix+"/orders").
 		WithQueryParams(filter.GetQuery()).
-		WithRateLimiter(a.RateLimitGetOrders).
 		Execute(a.HttpClient)
 }
 
 func (a *api) GetOrder(orderID string) (*apis.CallResponse[GetOrderResponse], error) {
 	return apis.NewCall[GetOrderResponse](http.MethodGet, pathPrefix+"/orders/"+orderID).
-		WithRateLimiter(a.RateLimitGetOrder).
 		Execute(a.HttpClient)
 }
 
 func (a *api) GetOrderBuyerInfo(orderID string) (*apis.CallResponse[GetOrderBuyerInfoResponse], error) {
 	return apis.NewCall[GetOrderBuyerInfoResponse](http.MethodGet, pathPrefix+"/orders/"+orderID+"/buyerInfo").
-		WithRateLimiter(a.RateLimitGetOrderBuyerInfo).
 		Execute(a.HttpClient)
 }
 
 func (a *api) GetOrderAddress(orderID string) (*apis.CallResponse[GetOrderAddressResponse], error) {
 	return apis.NewCall[GetOrderAddressResponse](http.MethodGet, pathPrefix+"/orders/"+orderID+"/address").
-		WithRateLimiter(a.RateLimitGetOrderAddress).
 		Execute(a.HttpClient)
 }
 
@@ -105,7 +81,6 @@ func (a *api) GetOrderItems(orderID string, nextToken *string) (*apis.CallRespon
 
 	return apis.NewCall[GetOrderItemsResponse](http.MethodGet, pathPrefix+"/orders/"+orderID+"/orderItems").
 		WithQueryParams(params).
-		WithRateLimiter(a.RateLimitGetOrderItems).
 		Execute(a.HttpClient)
 }
 
@@ -117,7 +92,6 @@ func (a *api) GetOrderItemsBuyerInfo(orderID string, nextToken *string) (*apis.C
 
 	return apis.NewCall[GetOrderItemsBuyerInfoResponse](http.MethodGet, pathPrefix+"/orders/"+orderID+"/orderItems/buyerInfo").
 		WithQueryParams(params).
-		WithRateLimiter(a.RateLimitGetOrderItemsBuyerInfo).
 		Execute(a.HttpClient)
 }
 
@@ -129,13 +103,11 @@ func (a *api) UpdateShipmentStatus(orderID string, payload *UpdateShipmentStatus
 
 	return apis.NewCall[UpdateShipmentStatusErrorResponse](http.MethodPost, pathPrefix+"/orders/"+orderID+"/shipment").
 		WithBody(body).
-		WithRateLimiter(a.RateLimitUpdateShipmentStatus).
 		Execute(a.HttpClient)
 }
 
 func (a *api) GetOrderRegulatedInfo(orderID string) (*apis.CallResponse[GetOrderRegulatedInfoResponse], error) {
 	return apis.NewCall[GetOrderRegulatedInfoResponse](http.MethodGet, pathPrefix+"/orders/"+orderID+"/regulatedInfo").
-		WithRateLimiter(a.RateLimitGetOrderRegulatedInfo).
 		Execute(a.HttpClient)
 }
 
@@ -147,6 +119,5 @@ func (a *api) UpdateVerificationStatus(orderID string, payload *UpdateVerificati
 
 	return apis.NewCall[UpdateVerificationStatusErrorResponse](http.MethodPatch, pathPrefix+"/orders/"+orderID+"/regulatedInfo").
 		WithBody(body).
-		WithRateLimiter(a.RateLimitUpdateVerificationStatus).
 		Execute(a.HttpClient)
 }
